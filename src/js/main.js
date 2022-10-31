42f6b9b8a0c51126d6e63a84500dd47d784035e2
lvaldenebro <lauravaldeal@gmail.com>
2022-10-31 11:04:02 +0100

    update page

--- src/js/main.js
+++ src/js/main.js
@@ -3,23 +3,67 @@
 const button = document.querySelector('.js-play-button');
 const userSelect = document.querySelector('.js-user-select');
 const winnerText = document.querySelector('.js-winner');
+const userContainer = document.querySelector('.js-user-container');
+const machineContainer = document.querySelector('.js-machine-container');
+const resetButton = document.querySelector('.js-reset-button');
 
 let userPlayPoints;
 let machinePlayPoints;
+let userCount = 0;
+let machineCount = 0;
+let clickCount = 0;
 
 //General code, not related with the event
 winnerText.innerHTML = "Comienza la batalla";
 
 //functions
 
-function compareWinner() {
+function handleReset() {
+	userCount = 0;
+	machineCount = 0;
+	printGlobalResult();
+	winnerText.innerHTML = "";
+	alternateGame();
+}
+
+function alternateGame() {
+	resetButton.classList.toggle("hidden");
+	button.classList.toggle("hidden");
+}
+
+function compareFinalWinner() {
+	if (userCount > machineCount) {
+		winnerText.innerHTML = "¡Ha ganado el Ejército del Bien!";
+		alternateGame();
+	} else {
+		winnerText.innerHTML = "¡Ha ganado el Ejército del Mal!";
+		alternateGame();
+	}
+}
+
+function endGame() {
+	if (clickCount === 10) {
+		compareFinalWinner();
+	};
+}
+
+function printGlobalResult() {
+	userContainer.innerHTML = userCount;
+	machineContainer.innerHTML = machineCount;
+}
+
+function compareRoundWinner() {
 	if (userPlayPoints === machinePlayPoints) {
 		winnerText.innerHTML = "Empate.";
 	}
 	else if (userPlayPoints > machinePlayPoints){
+		userCount += 1;
+		printGlobalResult();
 		winnerText.innerHTML = "Ha ganado el Ejército del Bien! Enhorabuena.";
 	}
 	else {
+		machineCount += 1;
+		printGlobalResult();
 		winnerText.innerHTML = "Ha ganado el Ejército del Mal! Vuelve a intentarlo.";
 	}
 }
@@ -45,11 +89,12 @@ function userPlay(input) {
 
 function handleClick(event) {
   event.preventDefault();
+	clickCount += 1;
 	userPlayPoints = userPlay(userSelect.value);
 	machinePlayPoints = getRandomNumber(6);
-	compareWinner();
-	console.log(userPlayPoints);
-	console.log(machinePlayPoints);
+	compareRoundWinner();
+	endGame();
 } 
 //events
 button.addEventListener('click', handleClick);
+resetButton.addEventListener('click', handleReset);
